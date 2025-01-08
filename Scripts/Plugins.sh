df2e6655d5e1d7c7ec1f4d4e5430ee8ae5e67c03
laomuyear <77942486+laomuyear@users.noreply.github.com>
2025-01-08 11:00:10 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #Design Theme
-git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "main" || echo "js") https://github.com/gngpp/luci-theme-design.git
-git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
-sed -i 's/dark/light/g' luci-app-design-config/root/etc/config/design
+#git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "main" || echo "js") https://github.com/gngpp/luci-theme-design.git
+#git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
+#sed -i 's/dark/light/g' luci-app-design-config/root/etc/config/design
 #Argon Theme
 #git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
 #git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-app-argon-config.git
-git clone https://github.com/jerrykuku/luci-theme-argon.git
+#git clone https://github.com/jerrykuku/luci-theme-argon.git
 #Linkease
 #git clone --depth=1 --single-branch https://github.com/linkease/istore.git
 #git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
@@ -19,7 +19,7 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git
 #git clone --depth=1 --single-branch --branch "main" https://github.com/xiaorouji/openwrt-passwall-packages.git ./pw_packages
 
 #fakemesh
-git clone https://github.com/x-wrt/com.x-wrt/tree/master/luci-app-fakemesh
+#git clone https://github.com/x-wrt/com.x-wrt/tree/master/luci-app-fakemesh
 
 #预置OpenClash内核和GEO数据
 #export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
