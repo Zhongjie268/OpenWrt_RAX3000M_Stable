47bc9431bc92eebbd4ad831786fe5ea856152f4c
laomuyear <77942486+laomuyear@users.noreply.github.com>
2025-01-08 10:40:11 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -7,8 +7,7 @@ sed -i 's/dark/light/g' luci-app-design-config/root/etc/config/design
 #Argon Theme
 #git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
 #git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-app-argon-config.git
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-theme-argon.git
 #Linkease
 #git clone --depth=1 --single-branch https://github.com/linkease/istore.git
 #git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
